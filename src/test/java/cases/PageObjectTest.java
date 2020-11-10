0fe762b70277bb9d772ca8af95546f38cf9d36be
Averyanova Elena <elenaa@hflabs.ru>
2020-11-10 16:16:09 +0300

    add lombok & logback

--- src/test/java/cases/PageObjectTest.java
+++ src/test/java/cases/PageObjectTest.java
@@ -1,13 +1,20 @@
 package cases;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import utils.BaseHooks;
 
+@Slf4j
 public class PageObjectTest extends BaseHooks {
+
+    private static final Logger logger = LoggerFactory.getLogger(PageObjectTest.class);
 
     @Test
     public void dummyOpenTest() {
         driver.get("https://events.epam.com/");
+        logger.info("Открыли сайт");
     }
 
 }
